6a3c2dff091d9129b4cbf6af62b54b67625740e2
AJ Ancheta and Marc Davies <marc@>
2019-05-29 13:20:08 -0700

    add three succesful tests for ping-pong js logic

--- spec/ping-pong-spec.js
+++ spec/ping-pong-spec.js
@@ -3,6 +3,17 @@ import { pingPong } from './../src/ping-pong.js';
 describe('pingPong', function() {
 
   it('should replace 3 with ping', function() {
-    expect(pingPong([3])).toEqual(1, 2, "ping");
+    var arr = [1, 2, "ping"];
+    expect(pingPong(3)).toEqual(arr);
+  });
+
+  it('should replace 5 with pong', function() {
+    var arr = [1, 2, "ping", 4, "pong"];
+    expect(pingPong(5)).toEqual(arr);
+  });
+
+  it('should replace 15 with ping-pong', function() {
+    var arr = [1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong"];
+    expect(pingPong(15)).toEqual(arr);
   });
 });
